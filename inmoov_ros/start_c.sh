52bdb403b1e54c91feb13a196f0feb77745f5fd6
creos92 <cesare.pec@gmail.com>
2018-03-06 11:28:34 +0100

    first

---
+++ inmoov_ros/start_c.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source ./devel/setup.bash
+chmod +rx start_play.sh /Client/certificati/client1/start_client.sh &&
+cd /Client/certificati/client1 && sync && ./start_client.sh &
+sleep 15s
+export ROS_IP=10.8.0.6
+export ROS_MASTER_URI=http://10.8.0.1:11311
+cd /inmoov_ros && ./start_play.sh
---
+++ inmoov_ros/start_talker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo Open traduction node
+sleep 10s
+source ./devel/setup.bash
+python ./src/beginner_tutorials/scripts/talker2.py
+
